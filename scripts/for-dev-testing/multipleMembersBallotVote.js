cf8e1e46ca9d1f2de412d02a84b05e1fd5557f20
medied <medina.eag@gmail.com>
2017-08-24 16:23:59 -0700

    Updated multiple members vote test to use winnerName()

--- scripts/for-dev-testing/multipleMembersBallotVote.js
+++ scripts/for-dev-testing/multipleMembersBallotVote.js
@@ -6,10 +6,11 @@ module.exports = function(callback) {
   // Initialize variables
   var firstMember = null; // Organization creator
   var secondMember = null;
+  var thirdMember = null;
   var ballot = null;
   var ballotAddress = null;
   var organization = null;
-  var proposals = ['yes', 'no'];
+  var proposals = ['yes, let us be 100% opensource', 'no, opensource is toxic'];
   var accounts = web3.eth.accounts;
 
   // Instantiate members
@@ -18,6 +19,9 @@ module.exports = function(callback) {
     return Member.new(accounts[1]);
   }).then(function(secondMemberInstance) {
     secondMember = secondMemberInstance;
+    return Member.new(accounts[2]);
+  }).then(function(thirdMemberInstance) {
+    thirdMember = thirdMemberInstance;
     // Then organization
     return Organization.new(accounts[0]);
   }).then(function(orgInstance) {
@@ -29,14 +33,16 @@ module.exports = function(callback) {
     ballotAddress = ballotInstance.address;
   }).then(function() {
     // Next add members to org
-    organization.addMember(accounts[1]) // analogous to addMember(secondMember.address)
+    organization.addMember(accounts[1]); // analogous to addMember(secondMember.address)
+    organization.addMember(accounts[2]);
     // Then members vote on ballot
     firstMember.vote(ballotAddress, 0);
     secondMember.vote(ballotAddress, 0);
+    thirdMember.vote(ballotAddress, 1);
     // Then finally we check ballot
-    return ballot.getProposalVoteCount(0);
-  }).then(function(returnedVoteCount) {
-    // There should be 2 votes for first proposal
-    console.log(returnedVoteCount.toNumber());
+    return ballot.winnerName();
+  }).then(function(winner) {
+    // There should be 2 votes for first proposal, 1 for the second
+    console.log('The winning proposal is: ', web3.toUtf8(winner));
   });
 }
