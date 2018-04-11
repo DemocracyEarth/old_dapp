f7b52473106eb900440bd5967c9534767f919886
medied <medina.eag@gmail.com>
2018-04-11 15:04:00 -0400

    Add getter and initial vote() version

--- contracts/LiquidDemocracy.sol
+++ contracts/LiquidDemocracy.sol
@@ -20,7 +20,7 @@ contract LiquidDemocracy {
     
     mapping(address => Voter) votersData;
 
-    // TODO - some kind of mapping for delegations 
+    // TODO - some kind of mapping for delegations
 
     function LiquidDemocracy(bytes32[] ballotInstance) public {
         for (uint i = 0; i < ballotInstance.length; i++) {
@@ -31,7 +31,6 @@ contract LiquidDemocracy {
         }
     }
 
-    // TODO - vote()
     // TODO - delegate()
     // TODO - revoke()
     
@@ -44,6 +43,29 @@ contract LiquidDemocracy {
     }
     
     /**
+    * @notice Getter for proposals length
+    */
+    function getBallotLength() public constant returns(uint length) {
+        return ballot.length;
+    }
+
+    /**
+    * @notice Getter for ballot option name
+    * @param proposal The desired proposal option
+    */
+    function getBallotName(uint proposal) public constant returns (bytes32 name) {
+        return ballot[proposal].name;
+    }
+
+    /**
+    * @notice Getter for ballot option vote count
+    * @param proposal The desired proposal option
+    */
+    function getBallotVoteCount(uint proposal) public constant returns (uint voteCount) {
+        return ballot[proposal].voteCount;
+    }
+
+    /**
     * @notice Register a new voter setting initial Voter elements
     */
     function registerNewVoter() public {
@@ -53,5 +75,22 @@ contract LiquidDemocracy {
 
         votersData[voterAddress].weight = 1;
         votersData[voterAddress].registered = true;
+    }
+
+    /**
+    * @notice TODO
+    * @param voter TODO
+    * @param voteOption TODO
+    */
+    function vote(address voter, uint voteOption) public {
+        require(votersData[voter].registered == true);
+        require(votersData[voter].voted != true);
+        
+        votersData[voter].voted = true;
+        
+        uint weight = votersData[voter].weight;
+
+        // Record vote for specific options
+        ballot[voteOption].voteCount += weight;
     }
 }
--- test/LiquidDemocracy.test.js
+++ test/LiquidDemocracy.test.js
@@ -18,9 +18,38 @@ contract('LiquidDemocracy', function (accounts){
       
       it('should register new voters', async function () {
         this.liquidDemocracyBallot = await LiquidDemocracy.new(ballot);
+        
         await this.liquidDemocracyBallot.registerNewVoter({ from: accounts[0] });
-        const voter1 = await this.liquidDemocracyBallot.getVoterData(accounts[0]);
+        const voter0 = await this.liquidDemocracyBallot.getVoterData(accounts[0]);
+        await this.liquidDemocracyBallot.registerNewVoter({ from: accounts[1] });
+        const voter1 = await this.liquidDemocracyBallot.getVoterData(accounts[1]);
+        await this.liquidDemocracyBallot.registerNewVoter({ from: accounts[2] });
+        const voter2 = await this.liquidDemocracyBallot.getVoterData(accounts[2]);
+        await this.liquidDemocracyBallot.registerNewVoter({ from: accounts[3] });
+        const voter3 = await this.liquidDemocracyBallot.getVoterData(accounts[3]);
+        
+        should.exist(voter0);
         should.exist(voter1);
+        should.exist(voter2);
+        should.exist(voter3);
+        // console.log(voter0);
+        // console.log(voter1);
+      });
+
+      it('should allow voters to vote', async function () {
+        // console.log(voter0);
+        this.liquidDemocracyBallot = await LiquidDemocracy.new(ballot);
+        
+        await this.liquidDemocracyBallot.registerNewVoter({ from: accounts[0] });
+
+        await this.liquidDemocracyBallot.vote(accounts[0], 0);
+
+        const voteCount = await this.liquidDemocracyBallot.getBallotVoteCount(0);
+        console.log(voteCount);
+
+        // TODO - should refactor to use beforeEach, 
+        // the setup should be taken care there,
+        // basically create ballot and register voters
       });
       
     });
