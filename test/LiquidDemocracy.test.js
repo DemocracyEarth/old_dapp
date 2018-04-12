9ac4a20b2c12633812dfebe26e32545da6bb6cef
medied <medina.eag@gmail.com>
2018-04-12 12:00:15 -0400

    Add voting tests

--- test/LiquidDemocracy.test.js
+++ test/LiquidDemocracy.test.js
@@ -1,3 +1,5 @@
+import assertRevert from './helpers/assertRevert';
+
 const LiquidDemocracy = artifacts.require('LiquidDemocracy');
 
 const BigNumber = web3.BigNumber;
@@ -16,17 +18,45 @@ contract('LiquidDemocracy', function (accounts){
     }
   });
   
-  it('should register voters successfully', async function () {
-    const registered = await this.liquidDemocracyBallot.getVoterData(accounts[0]);
-    let registeredWeight = registered[0].toString();
-    let registeredStatus = registered[1];
-    const unregistered = await this.liquidDemocracyBallot.getVoterData(accounts[4]);
-    let unregisteredWeight = unregistered[0].toString();
-    let unregisteredStatus = unregistered[1];
+  describe('registration set up', function () {
+    it('should register voters successfully', async function () {
+      const registered = await this.liquidDemocracyBallot.getVoterData(accounts[0]);
+      let registeredWeight = registered[0].toString();
+      let registeredStatus = registered[1];
+      const unregistered = await this.liquidDemocracyBallot.getVoterData(accounts[4]);
+      let unregisteredWeight = unregistered[0].toString();
+      let unregisteredStatus = unregistered[1];
+  
+      registeredWeight.should.equal('1');
+      registeredStatus.should.equal(true);
+      unregisteredWeight.should.equal('0');
+      unregisteredStatus.should.equal(false);
+    });
+  });
+  
+  describe('voting', function () {
+    it('should allow registered voters to vote', async function () {
+      await this.liquidDemocracyBallot.vote(accounts[0], 0);
+      await this.liquidDemocracyBallot.vote(accounts[1], 0);
+      await this.liquidDemocracyBallot.vote(accounts[2], 1);
+      const voteCountOption0 = await this.liquidDemocracyBallot.getBallotVoteCount(0);
+      const voteCountOption1 = await this.liquidDemocracyBallot.getBallotVoteCount(1);
 
-    registeredWeight.should.equal('1');
-    registeredStatus.should.equal(true);
-    unregisteredWeight.should.equal('0');
-    unregisteredStatus.should.equal(false);
-  });  
+      voteCountOption0.toNumber().should.equal(2);
+      voteCountOption1.toNumber().should.equal(1);
+    });
+
+    it('should not allow unregistered voters to vote', async function () {
+      await assertRevert(this.liquidDemocracyBallot.vote(accounts[4], 0));
+    });
+
+    it('should not allow registered voters to vote more than once', async function () {
+      await this.liquidDemocracyBallot.vote(accounts[0], 0);
+      await assertRevert(this.liquidDemocracyBallot.vote(accounts[0], 0));
+    });
+  });
+
+  describe('delegating', function () {
+    // TODO
+  });
 });
