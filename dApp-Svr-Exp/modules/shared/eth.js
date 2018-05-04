933bb5161176ad97801f75d722b0b1daf5090427
lucasisasmendi <lucas.isasmendi@gmail.com>
2018-05-04 15:32:42 +0100

    Rewrite module eth to ES8

--- dApp-Svr-Exp/modules/shared/eth.js
+++ dApp-Svr-Exp/modules/shared/eth.js
@@ -12,56 +12,44 @@
         web3Provider = web3.currentProvider;
     } else {
         console.info("Web3 instance not defined, connecting in read-only mode to the blockchain using an infura node")
-        web3Provider = new Web3.providers.HttpProvider('https://ropsten.infura.io/KZSQapS5wjr4Iw7JhgtE'); // TODO: Using @aecc's key to connect to a Ropsten node, change to our own node
+        // TODO: Using @aecc's key to connect to a Ropsten node, change to our own node
+        web3Provider = new Web3.providers.HttpProvider('https://ropsten.infura.io/KZSQapS5wjr4Iw7JhgtE');
     }
 
-    function getEthAccounts() {
+    function setEthDefaultAccount() {
       return new Promise((resolve, reject) => {
         try {
           const web3 = new Web3(web3Provider);
           web3.eth.getAccounts(function(error, accounts) {
             if (error) {
-                reject(error);
+              reject(error);
             }
-            var account = accounts[0];
+            const account = accounts[0];
             web3.eth.defaultAccount = account;
-            resolve();
+            resolve(web3.eth.defaultAccount);
           });
         } catch (e) {
           reject(e);
         }
       });
     }
-    
-    // Contract loading
-    function loadContract() {
-      return new Promise((resolve, reject) => {
-        $http.get('contracts/LiquidDemocracy.json').then(function(data){
-          // Get the necessary contract artifact file and instantiate it with truffle-contract
-          console.info("Loading json contract...: " + data);
-    
-          contract = TruffleContract(data.data);
-    
-          // Set the provider for our contract
-          contract.setProvider(web3Provider);
-          resolve(contract);
-          getEthAccounts();
-    
-        }, function(err){
-          reject(err);
-        });
-      });
-    };
 
-    function checkInstance(contract) {
-      return new Promise((resolve, reject) => {
-        // Simple API call to the contract
-        contract.deployed().then(function(instance) {
-          resolve(instance);
-        });
-      }, function(err){
-        reject(err);
-      });
+ 
+    // Contract loading
+    const loadContract = async () => {
+      const contractData = await $http.get('contracts/LiquidDemocracy.json');
+      console.info("Loading json contract...: ", contractData);
+      contract = TruffleContract(contractData.data);
+
+      // Set the provider for our contract
+      contract.setProvider(web3Provider);
+      const account = await setEthDefaultAccount();
+      console.log("account", account);
+      return contract;
+    }
+
+    const checkInstance = async (contract) => {
+      return await contract.deployed();
     }
     return {
       loadContract: loadContract,
